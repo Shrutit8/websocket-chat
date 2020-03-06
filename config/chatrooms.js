2ddfbbd9039b51c824bdce2380705cad3872c4f3
Shrutit8 <shrutit8@gmail.com>
2020-03-06 11:01:22 +0100

    Add files via upload

---
+++ config/chatrooms.js
@@ -0,0 +1,19 @@
+module.exports = [
+  {
+    name: 'Tektonikos',
+    image: 'chatrooms/company.jpg'
+  },
+  {
+    name: 'Help Desk',
+    image: 'chatrooms/helpdesk.jpg'
+  },
+  {
+    name: 'Public Forum',
+    image: 'chatrooms/public.jpg'
+  },
+  {
+    name: 'Confidential',
+    image: 'chatrooms/confidential.jpg'
+  }
+]
+
---
+++ config/users.js
@@ -0,0 +1,27 @@
+module.exports = [
+  {
+    name: 'Leonard',
+    lastName: 'Sandler',
+    statusText: 'How Can I Help you?',
+    image: 'users/Leonard.jpg'
+  },
+  {
+    name: 'Joe',
+    lastName: 'Sheldon',
+    statusText: 'Client Executive',
+    image: 'users/Joe.jpg'
+  },
+  {
+    name: 'Cassey',
+    lastName: 'Patrick',
+    statusText: 'Office Assitant',
+    image: 'users/Cassey.jpg'
+  },
+  {
+    name: 'Nile',
+    lastName: '',
+    statusText: 'Relationship Manager',
+    image: 'users/Nile.jpeg'
+  }
+]
+
